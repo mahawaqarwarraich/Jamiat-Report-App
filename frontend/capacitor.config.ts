712b5e6c1dc6e0e2daf3ce1e05ce143b4de32b20
Maha <mahawaqarwarraich@gmail.com>
2025-12-01 17:37:00 +0500

    cors final

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -1,11 +1,21 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.maha.jamireport',
+  appId: 'com.maha.jamiareport',
   appName: 'JamiaReport',
-  webDir: 'build'
+  webDir: 'build', // or 'build' depending on your setup
+  server: {
+    androidScheme: 'https',
+    cleartext: true, // Allow HTTP traffic for development //xml sy  bhi hatao
+    allowNavigation: [
+      'https://jamiat-report-app.vercel.app',
+      'localhost:5000',
+      'http://192.168.10.9:5000/api'
+    ]
+  },
+  android: {
+    allowMixedContent: true
+  }
 };
 
 export default config;
-
-//  "bundledWebRuntime": false
